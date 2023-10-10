236a4847bd03dd1af28bb20739b3c6b9891f4ab6
Saidqodirxon Rahimov <115058529+SaidqodirxonUz@users.noreply.github.com>
2023-10-10 12:33:20 +0500

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.createConnection(process.env.MONGODB_URI, {
-  // poolSize: 10,
-  // maxTimeMS: 1,
+// MongoDB ulanish qatorini o'zgartiring
+const MONGODB_URI = "mongodb+srv://rsaidqodirxon:3Osvy1qcE5F6kZSR@cluster0.niwu8aw.mongodb.net/?retryWrites=true&w=majority"; // MongoDB ulanish qatorini o'zgartiring
+
+const connection = mongoose.createConnection(MONGODB_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
+  // Boshqa xususiyatlar kerak bo'lsa ularni ham qo'shishingiz mumkin
+  // poolSize: 10,
+  // maxTimeMS: 1,
 });
 
 module.exports = connection;
